8a9aa2c03d096d2055fe19e018286a63251c5eef
M. Kashif Shamim <m.kashifshamim@Ms-MacBook-Pro.local>
2023-04-06 12:56:32 +0500

    Changed lspconfig, added Comment.nvim and .gitignore

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -20,16 +20,22 @@ packer.startup(function(use)
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
 
-  use 'neovim/nvim-lspconfig'                      -- LSP
-  use 'glepnir/lspsaga.nvim'                       -- LSP UIs
-  use 'onsails/lspkind-nvim'                       -- VSCode-like pictograms
-  use 'jose-elias-alvarez/null-ls.nvim'            -- Use Neovim as a language server to inject LSP diagnostic, code actions and more via Lua
-  use 'williamboman/mason.nvim'                    -- Installer for LSPs, DAPs, Linters & Formatters
-  use 'williamboman/mason-lspconfig.nvim'          -- Bridges Mason with lspconfig
+  use 'neovim/nvim-lspconfig'             -- LSP
+  use 'glepnir/lspsaga.nvim'              -- LSP UIs
+  use 'onsails/lspkind-nvim'              -- VSCode-like pictograms
+  use 'jose-elias-alvarez/null-ls.nvim'   -- Use Neovim as a language server to inject LSP diagnostic, code actions and more via Lua
+  use 'williamboman/mason.nvim'           -- Installer for LSPs, DAPs, Linters & Formatters
+  use 'williamboman/mason-lspconfig.nvim' -- Bridges Mason with lspconfig
 
-  use 'windwp/nvim-autopairs'                      -- Automatic brakets closing
-  use 'windwp/nvim-ts-autotag'                     -- Automatic treesitter tags closing
-  use 'fedepujol/move.nvim'                        -- Moving lines and blocks
+  use 'windwp/nvim-autopairs'             -- Automatic brakets closing
+  use 'windwp/nvim-ts-autotag'            -- Automatic treesitter tags closing
+  use 'fedepujol/move.nvim'               -- Moving lines and blocks
+  use {                                   -- Comments handling
+    'numToStr/Comment.nvim',
+    requires = {
+      'JoosepAlviste/nvim-ts-context-commentstring'
+    }
+  }
 
   use 'nvim-lua/plenary.nvim'                      -- Common utilities
   use 'nvim-telescope/telescope.nvim'              -- Fuzzy finder
@@ -44,8 +50,9 @@ end)
 
 -- TO DO
 --  [X] remove MunifTanjim/prettier.nvim and use default configs in null-ls
+--  [X] update lspconfig to setup native tailwindcss server
 --  [X] add move.nvim for moving blocks and lines
---  [] add numToStr/Comment.nvim & JoosepAlviste/nvim-ts-context-commentstring for comments
+--  [X] add numToStr/Comment.nvim & JoosepAlviste/nvim-ts-context-commentstring for comments
 --  [] add goolord/alpha-nvim as startup dashboard
 --  [] integrate akinsho/toggleterm of floating terminals
 --  [] add Lazygit config with toggleterm
--- nvim/plugin/lspconfig.rc.lua
+++ nvim/plugin/lspconfig.rc.lua
@@ -3,24 +3,74 @@ if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
 
-local on_attach = function(client, bufnr)
-  --formatting
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_command [[augroup Format]]
-    vim.api.nvim_command [[autocmd! * <buffer>]]
-    vim.api.nvim_command [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_seq_sync()]]
-    vim.api.nvim_command [[augroup END]]
-  end
+local augroup_format = vim.api.nvim_create_augroup("Format", { clear = true })
+local enable_format_on_save = function(_, bufnr)
+  vim.api.nvim_clear_autocmds({ group = augroup_format, buffer = bufnr })
+  vim.api.nvim_create_autocmd("BufWritePre", {
+    group = augroup_format,
+    buffer = bufnr,
+    callback = function()
+      vim.lsp.buf.format({ bufnr = bufnr })
+    end,
+  })
 end
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+
+  -- Mappings
+  local opts = { noremap = true, silent = true }
+
+  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+end
+
+protocol.CompletionItemKind = {
+  '', -- Text
+  '', -- Method
+  '', -- Function
+  '', -- Constructor
+  '', -- Field
+  '', -- Variable
+  '', -- Class
+  'ﰮ', -- Interface
+  '', -- Module
+  '', -- Property
+  '', -- Unit
+  '', -- Value
+  '', -- Enum
+  '', -- Keyword
+  '﬌', -- Snippet
+  '', -- Color
+  '', -- File
+  '', -- Reference
+  '', -- Folder
+  '', -- EnumMember
+  '', -- Constant
+  '', -- Struct
+  '', -- Event
+  'ﬦ', -- Operator
+  '', -- TypeParameter
+}
+
+-- Set up completion using nvim_cmp with LSP source
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-  cmd = { "typescript-language-server", "--stdio" }
+  cmd = { "typescript-language-server", "--stdio" },
+  capabilities = capabilities
 }
 
 nvim_lsp.lua_ls.setup {
-  on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = function(client, bufnr)
+    on_attach(client, bufnr)
+    enable_format_on_save(client, bufnr)
+  end,
   settings = {
     Lua = {
       runtime = {
@@ -46,6 +96,36 @@ nvim_lsp.lua_ls.setup {
 
 nvim_lsp.pyright.setup {
   on_attach = on_attach,
-  filetypes = { "python" },
-  cmd = { "pyright-langserver", "--stdio" }
+  capabilities = capabilities
 }
+
+nvim_lsp.tailwindcss.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    underline = true,
+    update_in_insert = false,
+    virtual_text = { spacing = 4, prefix = "●" },
+    severity_sort = true,
+  }
+)
+
+-- Diagnostics symbols in the sign column
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+vim.diagnostic.config({
+  virtual_text = {
+    prefix = '●',
+  },
+  update_in_insert = true,
+  float = {
+    source = "always", -- OR "if_many"
+  },
+})
